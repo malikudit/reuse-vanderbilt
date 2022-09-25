00c9113ada868fb6086b8183be0e4e623cc18e81
Ritvik Singh <ritvik.singh@vanderbilt.edu>
2022-09-25 01:30:30 -0500

    Added validation and error messages for user model

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -1,5 +1,6 @@
 const { DataTypes, Model } = require('sequelize');
 const sequelize = require('./database');
+const validator = require('validator');
 
 class User extends Model {
     
@@ -10,20 +11,32 @@ User.init({
         type: DataTypes.STRING(32),
         allowNull: false,
         validate: {
-            isAlpha: {
-                msg: ''
+            notNull: {
+                msg: 'First name is a required field'
             },
-            len: [2, 32]
+            isAlpha: {
+                msg: 'First name must be alphabetical'
+            },
+            len: {
+                args: [2, 32],
+                msg: 'First name must be between 2 to 32 characters long'
+            }
         }
     },
     lastName: {
         type: DataTypes.STRING(32),
         allowNull: false,
         validate: {
-            isAlpha: {
-                msg: ''
+            notNull: {
+                msg: 'Last name is a required field'
             },
-            len: [2, 32]
+            isAlpha: {
+                msg: 'Last name must be alphabetical'
+            },
+            len: {
+                args: [2, 32],
+                msg: 'Last name must be between 2 to 32 characters long'
+            }
         }
     },
     userName: {
@@ -31,20 +44,37 @@ User.init({
         allowNull: false,
         unique: true,
         validate: {
-            isAlphanumeric: {
-                msg: ''
+            notNull: {
+                msg: 'Username is a required field'
             },
-            len: [4, 32] // Look at this
+            isAlphanumeric: {
+                msg: 'Username must only contain alphabets and numbers'
+            },
+            len: {
+                args: [4, 32],
+                msg: 'Username must be between 4 to 32 characters long'
+            }
         }
     },
     email: {
-        type: DataTypes.STRING,
+        type: DataTypes.STRING(254),
         allowNull: false,
         primaryKey: true,
         unique: true,
         validate: {
-            isEmail: {
-                msg: ''
+            notNull: {
+                msg: 'Email address is a required field'
+            },
+            validateEmailAddress(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error('Email address is not well formed');
+                }
+            },
+            validateVanderbiltEmail(value) {
+                const lower_domain = value.split('@').pop().toLowerCase();
+                if (lower_domain !== 'vanderbilt.edu') {
+                    throw new Error('Please use a vanderbilt.edu email address to sign up');
+                }
             }
         }
     },
@@ -54,10 +84,10 @@ User.init({
         validate: {
             isIn: {
                 args: [['Cash', 'Venmo', 'Zelle', 'Other']],
-                msg: ''
+                msg: 'Payment method should be one of Cash, Venmo, Zelle or Other'
             },
             notNull: {
-                msg: 'Please enter your name'
+                msg: 'Payment method is a required field'
             }
         }
     },
@@ -67,31 +97,50 @@ User.init({
         validate: {
             isIn: {
                 args: [['Phone', 'GroupMe']],
-                msg: ''
+                msg: 'Preferred mode of communication must be either Phone or GroupMe'
             },
             notNull: {
-                msg: 'Please enter your name'
+                msg: 'Must specify a preferred mode of communication'
             }
         }
     },
-    // One of the two is required
     phoneNumber: {
-        type: DataTypes.STRING, // Validation needed
+        type: DataTypes.STRING(10),
         validate: {
             notNullIfPreferred(value) {
                 if (value === null && this.modeOfCommunication === 'Phone') {
-                    throw new Error('');
+                    throw new Error('A phone number is required if Phone is the preferred means of communication');
                 }
+            },
+            validatePhoneNumber(value) {
+                if (value !== null && !validator.isMobilePhone(value + '', 'en-US')) {
+                    throw new Error('A valid US phone number is required');
+                }
+            }
+        },
+        set(value) {
+            if (value === null) return;
+
+            const phoneNumber = (value + '').replaceAll(/[+\-()]/g, '');
+            if (phoneNumber[0] === '1') {
+                this.setDataValue('phoneNumber', phoneNumber.substring(1));
+            } else {
+                this.setDataValue('phoneNumber', phoneNumber);
             }
         }
     },
     groupMe: {
         type: DataTypes.STRING,
         validate: {
-            isUrl: true,
             notNullIfPreferred(value) {
                 if (value === null && this.modeOfCommunication === 'GroupMe') {
-                    throw new Error('');
+                    throw new Error('A GroupMe URL is required if GroupMe is the preferred means of communication');
+                }
+            },
+            validateGroupMeURL(value) {
+                if (value !== null && 
+                        !validator.isURL(value + '', { host_whitelist: ['groupme.com'] })) {
+                    throw new Error('The provided URL must be from groupme.com');
                 }
             }
         }
