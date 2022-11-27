468ef833216fc0e4a8116ae3bc7a478a3cc18140
“masonrange” <mason.rangee@gmail.com>
2022-11-27 11:43:43 -0600

    Reviews on Profile Page

--- front-end/src/pages/Profile.js
+++ front-end/src/pages/Profile.js
@@ -17,10 +17,7 @@ import DefaultBanner from "../components/DefaultBanner";
 import ReviewCards from "../components/ReviewCards";
 import { SampleReviews } from "../content/SampleReviews";
 import "./Profile.css";
-import {
-  paymentMethods,
-  formsOfContact,
-} from "../content/ProfilePreferences";
+import { paymentMethods, formsOfContact } from "../content/ProfilePreferences";
 import Parwaz from "../assets/Parwaz.png";
 import Bike from "../assets/Bike.jpg";
 import isMobilePhone from "validator/es/lib/isMobilePhone";
@@ -95,23 +92,22 @@ export default function Profile() {
     setSaved(false);
   };
   const handleSave = () => {
-
-    if (
-      checkNameLength(firstName) ||
-      checkNameAlpha(firstName)
-    ) {
+    if (checkNameLength(firstName) || checkNameAlpha(firstName)) {
       firstNameError = true;
     }
-    if (
-      checkNameLength(lastName) ||
-      checkNameAlpha(lastName)
-    ) {
+    if (checkNameLength(lastName) || checkNameAlpha(lastName)) {
       lastNameError = true;
     }
-    if ((contact === "Phone" || contact === "Any") && checkPhoneNumber(phoneNumber)) {
+    if (
+      (contact === "Phone" || contact === "Any") &&
+      checkPhoneNumber(phoneNumber)
+    ) {
       phoneNumberError = true;
     }
-    if ((contact === "GroupMe" || contact === "Any") && checkGroupMeURL(groupMe)) {
+    if (
+      (contact === "GroupMe" || contact === "Any") &&
+      checkGroupMeURL(groupMe)
+    ) {
       groupMeError = true;
     }
     if (firstNameError || lastNameError || phoneNumberError || groupMeError) {
@@ -141,7 +137,8 @@ export default function Profile() {
                 <div class="profile-usertitle">
                   <div class="profile-usertitle-name">
                     <TextField
-                      width="auto" label="First Name"
+                      width="auto"
+                      label="First Name"
                       variant={saved ? "standard" : "standard"}
                       disabled={saved}
                       onChange={(event) => {
@@ -151,21 +148,25 @@ export default function Profile() {
                       error={
                         (checkNameEmpty(firstName) ||
                           checkNameLength(firstName) ||
-                          checkNameAlpha(firstName)) && saved === false
+                          checkNameAlpha(firstName)) &&
+                        saved === false
                       }
                       helperText={
-                        saved === true ? ""
+                        saved === true
+                          ? ""
                           : checkNameLength(firstName)
-                            ? "First name must be between 2-32 characters"
-                            : checkNameAlpha(firstName)
-                              ? "First name must be alphabetical"
-                              : ""
-                      } />
+                          ? "First name must be between 2-32 characters"
+                          : checkNameAlpha(firstName)
+                          ? "First name must be alphabetical"
+                          : ""
+                      }
+                    />
                   </div>
 
                   <div class="profile-usertitle-name">
                     <TextField
-                      width="auto" label="Last Name"
+                      width="auto"
+                      label="Last Name"
                       variant={saved ? "standard" : "standard"}
                       disabled={saved}
                       onChange={(event) => {
@@ -175,16 +176,19 @@ export default function Profile() {
                       error={
                         (checkNameEmpty(lastName) ||
                           checkNameLength(lastName) ||
-                          checkNameAlpha(lastName)) && saved === false
+                          checkNameAlpha(lastName)) &&
+                        saved === false
                       }
                       helperText={
-                        saved === true ? ""
+                        saved === true
+                          ? ""
                           : checkNameLength(lastName)
-                            ? "Last name must be between 2-32 characters"
-                            : checkNameAlpha(lastName)
-                              ? "Last name must be alphabetical"
-                              : ""
-                      } />
+                          ? "Last name must be between 2-32 characters"
+                          : checkNameAlpha(lastName)
+                          ? "Last name must be alphabetical"
+                          : ""
+                      }
+                    />
                   </div>
                 </div>
                 <div class="profile-userbuttons">
@@ -192,15 +196,25 @@ export default function Profile() {
                     <Button
                       variant="contained"
                       component="label"
-                      onClick={() => { handleEdit(); }}
+                      onClick={() => {
+                        handleEdit();
+                      }}
                       sx={{
                         marginTop: "1vh",
-                        marginBottom: "3vh"
-                      }}>
+                        marginBottom: "3vh",
+                      }}
+                    >
                       Edit Profile Details
                     </Button>
                   ) : (
-                    <Button variant="contained" color="success" component="label" onClick={() => { handleSave(); }}>
+                    <Button
+                      variant="contained"
+                      color="success"
+                      component="label"
+                      onClick={() => {
+                        handleSave();
+                      }}
+                    >
                       Save Changes
                     </Button>
                   )}
@@ -216,19 +230,23 @@ export default function Profile() {
                       disabled
                       value="udit.malik@vanderbilt.edu"
                       sx={{
-                        marginTop: "1vh"
+                        marginTop: "1vh",
                       }}
                     />
 
-                    <FormControl fullWidth
+                    <FormControl
+                      fullWidth
                       sx={{
-                        marginTop: "1vh"
-                      }}>
-                      <InputLabel id="demo-simple-select-label"
+                        marginTop: "1vh",
+                      }}
+                    >
+                      <InputLabel
+                        id="demo-simple-select-label"
                         sx={{
                           marginLeft: "-1vw",
-                          marginTop: "1vh"
-                        }}>
+                          marginTop: "1vh",
+                        }}
+                      >
                         Preferred form(s) of payment
                       </InputLabel>
                       <Select
@@ -241,7 +259,7 @@ export default function Profile() {
                         onChange={preferredPaymentHandler}
                         value={preferredPayment}
                         sx={{
-                          marginTop: "1vh"
+                          marginTop: "1vh",
                         }}
                         renderValue={(preferredPayment) => (
                           <div>
@@ -249,10 +267,10 @@ export default function Profile() {
                               <Chip key={value} label={value} />
                             ))}
                           </div>
-                        )}>
+                        )}
+                      >
                         {paymentMethods.map((option) => (
-                          <MenuItem key={option.value}
-                            value={option.value}>
+                          <MenuItem key={option.value} value={option.value}>
                             {option.label}
                           </MenuItem>
                         ))}
@@ -270,13 +288,11 @@ export default function Profile() {
                       }}
                       value={contact}
                       sx={{
-                        marginTop: "1vh"
+                        marginTop: "1vh",
                       }}
                     >
                       {formsOfContact.map((option) => (
-                        <MenuItem
-                          key={option.value}
-                          value={option.value}>
+                        <MenuItem key={option.value} value={option.value}>
                           {option.label}
                         </MenuItem>
                       ))}
@@ -299,11 +315,11 @@ export default function Profile() {
                             saved === true
                               ? ""
                               : checkPhoneNumber(phoneNumber)
-                                ? "Enter a valid US phone number"
-                                : ""
+                              ? "Enter a valid US phone number"
+                              : ""
                           }
                           sx={{
-                            marginTop: "1vh"
+                            marginTop: "1vh",
                           }}
                           value={phoneNumber}
                         />
@@ -315,74 +331,65 @@ export default function Profile() {
                           onChange={(event) => {
                             setGroupMe(event.target.value);
                           }}
-                          error={
-                            checkGroupMeURL(groupMe) && saved === false
-                          }
+                          error={checkGroupMeURL(groupMe) && saved === false}
                           helperText={
                             saved === true
                               ? ""
                               : checkGroupMeURL(groupMe)
-                                ? "The provided URL must be from groupme.com and use https"
-                                : ""
+                              ? "The provided URL must be from groupme.com and use https"
+                              : ""
                           }
                           value={groupMe}
                           sx={{
-                            marginTop: "1vh"
-                          }} />
-                      </Grid>
-
-                    ) :
-
-                      contact === "Phone" ? (
-                        <TextField
-                          fullWidth
-                          label="Phone Number"
-                          variant={saved ? "standard" : "standard"}
-                          disabled={saved}
-                          onChange={(event) => {
-                            setPhoneNumber(event.target.value);
+                            marginTop: "1vh",
                           }}
-                          error={
-                            checkPhoneNumber(phoneNumber) && saved === false
-                          }
-                          helperText={
-                            saved === true
-                              ? ""
-                              : checkPhoneNumber(phoneNumber)
-                                ? "Enter a valid US phone number"
-                                : ""
-                          }
-                          sx={{
-                            marginTop: "1vh"
-                          }}
-                          value={phoneNumber}
                         />
-
-                      ) : contact === "GroupMe" ? (
-
-                        <TextField
-                          fullWidth
-                          label="GroupMe URL"
-                          variant={saved ? "standard" : "standard"}
-                          disabled={saved}
-                          onChange={(event) => {
-                            setGroupMe(event.target.value);
-                          }}
-                          error={
-                            checkGroupMeURL(groupMe) && saved === false
-                          }
-                          helperText={
-                            saved === true
-                              ? ""
-                              : checkGroupMeURL(groupMe)
-                                ? "The provided URL must be from groupme.com and use https"
-                                : ""
-                          }
-                          sx={{
-                            marginTop: "1vh"
-                          }}
-                          value={groupMe} />
-                      ) : null}
+                      </Grid>
+                    ) : contact === "Phone" ? (
+                      <TextField
+                        fullWidth
+                        label="Phone Number"
+                        variant={saved ? "standard" : "standard"}
+                        disabled={saved}
+                        onChange={(event) => {
+                          setPhoneNumber(event.target.value);
+                        }}
+                        error={checkPhoneNumber(phoneNumber) && saved === false}
+                        helperText={
+                          saved === true
+                            ? ""
+                            : checkPhoneNumber(phoneNumber)
+                            ? "Enter a valid US phone number"
+                            : ""
+                        }
+                        sx={{
+                          marginTop: "1vh",
+                        }}
+                        value={phoneNumber}
+                      />
+                    ) : contact === "GroupMe" ? (
+                      <TextField
+                        fullWidth
+                        label="GroupMe URL"
+                        variant={saved ? "standard" : "standard"}
+                        disabled={saved}
+                        onChange={(event) => {
+                          setGroupMe(event.target.value);
+                        }}
+                        error={checkGroupMeURL(groupMe) && saved === false}
+                        helperText={
+                          saved === true
+                            ? ""
+                            : checkGroupMeURL(groupMe)
+                            ? "The provided URL must be from groupme.com and use https"
+                            : ""
+                        }
+                        sx={{
+                          marginTop: "1vh",
+                        }}
+                        value={groupMe}
+                      />
+                    ) : null}
                   </div>
                 </div>
               </div>
@@ -390,15 +397,20 @@ export default function Profile() {
           </form>
 
           <Grid>
-            <div class="profile-content">
-            </div>
+            <div class="profile-content"></div>
           </Grid>
 
           <Grid>
             <div class="profile-content">
               <div>
                 <h3 class="profile-desc-title">Review by Udit Malik</h3>
-                <span class="profile-desc-text"> Lorem ipsum dolor sit amet diam nonummy nibh dolore. </span>
+                <span class="profile-desc-text">
+                  {" "}
+                  Lorem ipsum dolor sit amet diam nonummy nibh dolore.{" "}
+                  {SampleReviews.map((review) => (
+                    <ReviewCards {...review} key={review.itemName} />
+                  ))}
+                </span>
               </div>
             </div>
           </Grid>
@@ -415,21 +427,28 @@ export default function Profile() {
                     </div>
                     <div class="col-md-4 col-sm-4 col-xs-6">
                       <div class="uppercase profile-stat-title"> 51 </div>
-                      <div class="uppercase profile-stat-text"> Products Sold </div>
+                      <div class="uppercase profile-stat-text">
+                        {" "}
+                        Products Sold{" "}
+                      </div>
                     </div>
                     <div class="col-md-4 col-sm-4 col-xs-6">
                       <div class="uppercase profile-stat-title"> 61 </div>
-                      <div class="uppercase profile-stat-text"> Products Bought </div>
+                      <div class="uppercase profile-stat-text">
+                        {" "}
+                        Products Bought{" "}
+                      </div>
                     </div>
                     <div class="col-md-4 col-sm-4 col-xs-6">
                       <div class="uppercase profile-stat-title"> 3.77 </div>
-                      <div class="uppercase profile-stat-text"> Average Rating </div>
+                      <div class="uppercase profile-stat-text">
+                        {" "}
+                        Average Rating{" "}
+                      </div>
                     </div>
                     <div class="col-md-4 col-sm-4 col-xs-6">
                       <div class="uppercase profile-stat-title">
-                        <Button>
-                          View All Reviews
-                        </Button>
+                        <Button>View All Reviews</Button>
                       </div>
                     </div>
                   </div>
@@ -437,15 +456,9 @@ export default function Profile() {
               </div>
             </div>
           </Grid>
-
-
-
-
         </div>
-
-
       </ThemeProvider>
-    </div >
+    </div>
 
     //               <Grid xs={12} marginTop={4}>
     //                 <Box sx={{ boxShadow: "0 0 5px #ccc", }}>
