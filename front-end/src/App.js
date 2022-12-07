1c58a9d980f0a442d692b873af28dfbda3de82ab
“masonrange” <mason.rangee@gmail.com>
2022-12-07 10:49:48 -0600

    React Helmet Tag and Edit Profile Patch

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,28 @@
-import { React, useState } from "react";
-import NavBar from "./components/NavBar.js";
-import Footer from "./components/Footer.js";
-import MainRoutes from "./MainRoutes.js";
-import "./App.css";
+import { React, useState } from 'react';
+import { Helmet } from 'react-helmet';
+import NavBar from './components/NavBar.js';
+import Footer from './components/Footer.js';
+import MainRoutes from './MainRoutes.js';
+import './App.css';
 
 function App() {
-  const [searchProduct, setSearchProduct] = useState([""]);
+  const [searchProduct, setSearchProduct] = useState(['']);
   const [categoryProduct, setCategoryProduct] = useState([]);
 
   return (
     <>
+      <div className="application">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Reuse Vandy</title>
+          <link rel="canonical" href="https://reusevandy.org/" />
+          <meta
+            name="description"
+            content="Reuse Vandy - A Vanderbilt Marketplace"
+          />
+        </Helmet>
+      </div>
+      );
       <NavBar />
       <MainRoutes
         searchProduct={searchProduct}
